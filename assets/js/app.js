f124976d08a2ed496ea73e69ee9a49c936ab2927
karnamin <aminkarn18@gmail.com>
2019-04-28 21:09:27 +0100

    fixes nav-bar on mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,31 @@
+// Get the navbar
+const navigation = document.getElementById("navigation-bar");
+
+// Get the offset position of the navbar
+/* var sticky = navigation.offsetTop; */
+const sticky = navigation.offsetHeight;
+console.log(sticky);
+
+const smallDevices = window.matchMedia("(max-width: 991px)");
+
+const bigDevices = window.matchMedia("(min-width: 992px)");
+
+window.addEventListener("load", navBarSticky());
+
 // When the user scrolls the page, execute scrollFunction
 window.onscroll = function() {
     scrollFunction();
 };
 
-// Get the navbar
-let navigation = document.getElementById("navigation-bar");
-
-// Get the offset position of the navbar
-/* var sticky = navigation.offsetTop; */
-let sticky = navigation.offsetHeight;
-console.log(sticky);
-
-let mediaQuery = window.matchMedia("(max-width: 991px)");
-
-window.addEventListener("load", navBarSticky());
-
 function navBarSticky() {
-    if (mediaQuery.matches) {
+    if (smallDevices.matches) {
         navigation.classList.remove("sticky");
     }
 }
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function scrollFunction() {
-    if (window.pageYOffset >= 510) {
+    if (bigDevices.matches && window.pageYOffset >= 510) {
         navigation.classList.add("sticky");
     } else {
         navigation.classList.remove("sticky");
