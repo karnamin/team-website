3d479f003ba4e4c71c40e4ce6a4ac107b32640be
karnamin <aminkarn18@gmail.com>
2019-04-28 01:11:34 +0100

    more fixes to navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-// When the user scrolls the page, execute myFunction
+// When the user scrolls the page, execute scrollFunction
 window.onscroll = function() {
-    myFunction();
+    scrollFunction();
 };
 
 // Get the navbar
@@ -11,8 +11,18 @@ let navigation = document.getElementById("navigation-bar");
 let sticky = navigation.offsetHeight;
 console.log(sticky);
 
+let mediaQuery = window.matchMedia("(max-width: 991px)");
+
+window.addEventListener("load", navBarSticky());
+
+function navBarSticky() {
+    if (mediaQuery.matches) {
+        navigation.classList.remove("sticky");
+    }
+}
+
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function scrollFunction() {
     if (window.pageYOffset >= 510) {
         navigation.classList.add("sticky");
     } else {
